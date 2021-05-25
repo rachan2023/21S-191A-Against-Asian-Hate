2bdfa87dc158b69072a50a39e67c3eee30b43cef
rachan2023 <rachan2023@g.ucla.edu>
2021-05-25 00:06:44 -0700

    age  has own marker color, main scroll broken

--- Final project/js/init.js
+++ Final project/js/init.js
@@ -7,6 +7,9 @@ let CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles
 });
 
 CartoDB_Voyager.addTo(map)
+// create a new global scoped variable called 'scroller'
+// you can think of this like the "map" with leaflet (i.e. const map = L.map('map'))
+// let scroller = scrollama();
 
 //marker cluster group 
 let mcg = L.markerClusterGroup({
@@ -57,11 +60,11 @@ function addMarker(data){
         let city = data.whatcitydoyouorthepersonyouarerepresentingcurrentlylivein
         let age = data.howoldareyouorthepersonyouarerepresenting
         createButtons(data.lat,data.lng,city)
-        getDistinctValues(myFieldArray)
+        getDistinctValues(age)
         console.log('all the distinct fields')
         console.log(myFieldArray)
         colorArray = ['green','blue','red','purple']
-        circleOptions.fillColor = colorArray[myFieldArray.indexOf(myFieldArray)]
+        circleOptions.fillColor = colorArray[myFieldArray.indexOf(age)]
         console.log("age")
         console.log(age)
         if (age == "under 59"){         
@@ -73,7 +76,7 @@ function addMarker(data){
             sixtyfour.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${city}under 59 yrs old</h2> `))
         }
         else if (age == "65-69") {
-            sixynine.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${city}under 59 yrs old</h2>`))
+            sixtynine.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${city}under 59 yrs old</h2>`))
         }
         else {
             overseventy.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>${city}under 59 yrs old</h2> `))
@@ -86,6 +89,8 @@ function createButtons(lat,lng,anything){
     const newButton = document.createElement("button"); // adds a new button
     newButton.id = "button"+anything; // gives the button a unique id
     newButton.innerHTML = anything; // gives the button a title
+    // newButton.setAttribute("class","step") // add the class called "step" to the button or div
+    // newButton.setAttribute("data-step",newButton.id) // add a data-step for the button id to know which step we are on
     newButton.setAttribute("lat",lat); // sets the latitude 
     newButton.setAttribute("lng",lng); // sets the longitude 
     newButton.addEventListener('click', function(){
@@ -120,14 +125,39 @@ function formatData(theData){
         map.fitBounds(allLayers.getBounds()); 
         startModal()
         document.getElementById("myBtn").click() // simulate click to start modal
+        // setup the instance, pass callback functions
+        // use the scrollama scroller variable to set it up
+        // scroller
+        // .setup({
+        //     step: ".step", // this is the name of the class that we are using to step into, it is called "step", not very original
+        // })
+        // // do something when you enter a "step":
+        // .onStepEnter((response) => {
+        //     // you can access these objects: { element, index, direction }
+        //     // use the function to use element attributes of the button 
+        //     // it contains the lat/lng: 
+        //     scrollStepper(response.element.attributes)
+        // })
+        // .onStepExit((response) => {
+        //     // { element, index, direction }
+        //     // left this in case you want something to happen when someone
+        //     // steps out of a div to know what story they are on.
+        // });
 }
-
+// function scrollStepper(thisStep){
+//     // optional: console log the step data attributes:
+//     // console.log("you are in thisStep: "+thisStep)
+//     let thisLat = thisStep.lat.value
+//     let thisLng = thisStep.lng.value
+//     // tell the map to fly to this step's lat/lng pair:
+//     map.flyTo([thisLat,thisLng])
+// }
 let layers = {
 
 	'<i style="background: green; border-radius: 50%;"></i> Under 59 yrs old ': under59,
-	'<i style="background: blue; border-radius: 50%;"></i> 60-64 yrs old': sixtyfour,
-    '<i style="background: red; border-radius: 50%;"></i>65-69 yrs old': sixtynine,
-	'<i style="background: purple; border-radius: 50%;"></i> Over 70 yrs old': overseventy
+	'<i style="background: red; border-radius: 50%;"></i> 60-64 yrs old': sixtyfour,
+    '<i style="background: purple; border-radius: 50%;"></i>65-69 yrs old': sixtynine,
+	'<i style="background: blue; border-radius: 50%;"></i> Over 70 yrs old': overseventy
 }
 
 L.control.layers(null,layers,{collapsed: false}).addTo(map)
@@ -172,3 +202,5 @@ function startModal(){
       }
     }
 }
+// setup resize event for scrollama incase someone wants to resize the page...
+// window.addEventListener("resize", scroller.resize);
