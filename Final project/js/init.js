94e356f32a7638f8f9380e38a66fb9bd7d9a981c
rachan2023 <rachan2023@g.ucla.edu>
2021-05-27 02:44:58 -0700

    added age and fearful info to card

--- Final project/js/init.js
+++ Final project/js/init.js
@@ -122,7 +122,7 @@ function addMarker(thisData){
 function createButtons(lat,lng,data){
     const newDiv = document.createElement("div"); // adds a new button
     
-    let cardContent =  `<h2>${data.city}</h2> <div class='story'> <p> Age: ${data.age} </p> <p>Fearful of going outside: ${data.fear} </p>  Reason: ${data.story}</div>`
+    let cardContent =  `<h2>${data.city}</h2> <div class='story'> <p> Age: ${data.age} </p> <p>Fearful of going outside: ${data.fear} </p>  Story: ${data.story}</div>`
     newDiv.id = "button"+data.story; // gives the button a unique id
     newDiv.innerHTML = cardContent; // gives it the HTML content
     newDiv.setAttribute("class","card") // add the class called "step" to the button or div
