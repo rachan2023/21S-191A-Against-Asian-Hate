9e4dfd2c0b7c39bf16052f2809e89c646eb08585
rachan2023 <rachan2023@g.ucla.edu>
2021-06-01 18:19:12 -0700

    remove mcg

--- Final project/js/init.js
+++ Final project/js/init.js
@@ -73,6 +73,7 @@ function onEachFeature(feature, layer) {
         console.log(count)
         let text = count.toString()
         layer.bindPopup(text);
+
     }
 }
 function getStyles(data){
@@ -310,7 +311,7 @@ function formatData(theData){
         allLayers = L.featureGroup([under59, sixtyfour, sixtynine, overseventy]);
         thePoints = turf.featureCollection(allPoints)
         getBoundary(boundaryLayer)
-        mcg.addTo(map)
+        //mcg.addTo(map)
         // console.log(allLayers)
         allLayers.addTo(map)
         map.fitBounds(allLayers.getBounds()); 
