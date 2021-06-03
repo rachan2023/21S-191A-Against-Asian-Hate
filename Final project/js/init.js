65b1921f4a32881377cd952f9c997f89be813a1d
rachan2023 <rachan2023@g.ucla.edu>
2021-06-03 02:04:34 -0700

    adding files for word cloud Co-authored-by: albertkun <albertkun@users.noreply.github.com>

--- Final project/js/init.js
+++ Final project/js/init.js
@@ -1,3 +1,5 @@
+// import stopwords from "./stopwords.js"
+
 const map = L.map('map').setView([34.0709, -118.444], 5);
 
 let CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
@@ -45,7 +47,9 @@ let circleOptions = {
 
 let boundaryLayer = "./data/la_county_zipcodes.geojson"
 let flag;
-
+function countiesF() {
+    // have the map show counties that are fearful
+}
 
 // function toggleBoundaries(flag){
 //     if (flag == "zip"){
@@ -201,13 +205,13 @@ function addMarker(thisData){
        
         return data   
 }
-
+let storyID = 1
 function createButtons(lat,lng,data){
     const newDiv = document.createElement("div"); // adds a new button
     
     if (data.age != "under 59" && data.gender == "Woman") {
         let cardContent =  `<h2> Elderly Woman </h2> <div class='story'> <p> Age: ${data.age} </p> <p> Location: ${data.city} </p> <p> Asian American/Pacific Islander: ${data.iden} </p> <p> Gender identity: ${data.gender} </p> <p>Fearful of going outside: ${data.fear} </p>  Story: ${data.story}</div>`
-        newDiv.id = "button"+data.story; // gives the button a unique id
+        newDiv.id = "button"+storyID; // gives the button a unique id
         newDiv.innerHTML = cardContent; // gives it the HTML content
         newDiv.setAttribute("class","card") // add the class called "step" to the button or div
         newDiv.setAttribute("data-step",newDiv.id) // add a data-step for the button id to know which step we are on
@@ -226,7 +230,7 @@ function createButtons(lat,lng,data){
     }
     else if (data.age == "under 59" && data.gender == "Woman") {
         let cardContent =  `<h2> Woman </h2> <div class='story'> <p> Age: ${data.age} </p><p> Location: ${data.city} </p> <p> Asian American/Pacific Islander: ${data.iden} </p> <p> Gender identity: ${data.gender} </p> <p>Fearful of going outside: ${data.fear} </p>  Story: ${data.story}</div>`
-        newDiv.id = "button"+data.story; // gives the button a unique id
+        newDiv.id = "button"+storyID; // gives the button a unique id
         newDiv.innerHTML = cardContent; // gives it the HTML content
         newDiv.setAttribute("class","card") // add the class called "step" to the button or div
         newDiv.setAttribute("data-step",newDiv.id) // add a data-step for the button id to know which step we are on
@@ -245,7 +249,7 @@ function createButtons(lat,lng,data){
     }
     else if (data.gender != "Woman" && data.age == "under 59"){
         let cardContent = `<h2> Asian American Pacific Islander </h2> <div class='story'> <p> Age: ${data.age} </p> <p> Location: ${data.city} </p> <p> Asian American/Pacific Islander: ${data.iden} </p> <p> Gender identity: ${data.gender} </p> <p>Fearful of going outside: ${data.fear} </p>  Story: ${data.story}</div>`
-        newDiv.id = "button"+data.story; // gives the button a unique id
+        newDiv.id = "button"+storyID; // gives the button a unique id
         newDiv.innerHTML = cardContent; // gives it the HTML content
         newDiv.setAttribute("class","card") // add the class called "step" to the button or div
         newDiv.setAttribute("data-step",newDiv.id) // add a data-step for the button id to know which step we are on
@@ -280,9 +284,25 @@ function createButtons(lat,lng,data){
                 ); //this is the flyTo from Leaflet
 
         })
-
     }
     const spaceForButtons = document.getElementById('contents')
+    console.log(data.story)
+    let cloudText = []
+    let thisWord;
+    let storyWords = data.story.split(" ")
+    storyWords.forEach(word => thisWord = stopWordCheck(word))
+    function stopWordCheck(word){
+        if (!stopwords.includes(word.toLowerCase())){
+            cloudText.push(word)
+        }
+    }
+    //ToDo: The button1 ID is hard coded, you'd probably want to have a space just for the tagCloud or have a space per card for it
+    // I was having issues getting a TagCloud per card though....
+    // I'll let you try to see if you can implement the wordcloud per card!
+    TagCloud('#button1', cloudText)
+
+    newDiv.innerHTML += "<div class='tagCloud'></div>";
+    storyID+=1;
     spaceForButtons.appendChild(newDiv);//this adds the button to our page.
 }
 
@@ -400,5 +420,5 @@ function startModal(){
     }
 }
 // setup resize event for scrollama incase someone wants to resize the page...
-
 window.addEventListener("resize", scroller.resize);
+
